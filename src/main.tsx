e9f31c678701731bb2d47d84ed5cf4e3e36b50ac
mateenmohiuddin <mohiuddin.mateen@gmail.com>
2026-01-26 21:09:50 -0600

    authentication changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
 
-Amplify.configure(outputs);
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "us-east-1_Nj7xV6STB",
+      userPoolClientId: "11ohlraq9la3mbhjtk5kl4jv5a",
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <App />
